a82e4b5e205bcd8c43077405957d0348b66b2434
Fr Labo <facha@laboAl>
2019-05-21 20:25:22 -0300

    clase7

--- Clase7.hs
+++ Clase7.hs
@@ -1,9 +1,4 @@
-head [(1,2), (3,4), (5,2)]
-tail [1,2,3,4,4,3,2,1]
-head []
-head [1,2,3] : [2,3]
-[True, True] ++ [False, False]
-[1,2] : []
+
 
 -- Definir la función listar :: a -> a -> a -> [a] que toma 3 elementos y los convierte en una lista.
 -- Escribir una expresión que denote la lista estrictamente decreciente de enteros que comienza con el número 1 y termina con el número -100.
