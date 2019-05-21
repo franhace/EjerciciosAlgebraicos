e9a278353d25a634328f0c62631d6b50248beb5d
mrfranac <13649860+franhace@users.noreply.github.com>
2019-05-21 09:40:14 -0300

    clase7

--- clase7.hs
+++ clase7.hs
@@ -3,12 +3,12 @@
 
 listita = [2, 3, 4, 5, 3, 2, 9, 12]
 
---Definir la función listar :: a -> a -> a -> [a] que toma 3 elementos y los convierte
+--Ej: Definir la función listar :: a -> a -> a -> [a] que toma 3 elementos y los convierte
 --en una lista
 listar :: a -> a -> a -> [a]
 listar a b c = [a, b, c]
 
---Definir la función pertenece :: a -> [a] -> Bool que indica si el elemento que se le pasa
+--Ej: Definir la función pertenece :: a -> [a] -> Bool que indica si el elemento que se le pasa
 --está o no en la lista.
 
 -- Forma 1
@@ -19,8 +19,39 @@ pertenece :: Integer -> [Integer] -> Bool
 pertenece _ [] = False
 pertenece b (x:xs) = (b == x) || pertenece b xs
 
--- sumatoria :: [Integer] -> Integer
+-- Ej: sumatoria :: [Integer] -> Integer
 -- que indica la suma de los elementos de una lista.
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+-- Funcion que le suma un numero a cada valor de una lista
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN _ [] = []
+sumarN n (x:xs) = (x-1):(sumarN n xs)
+
+-- Funcion que suma 1 a cada valor de una lista
+crece :: [Integer] -> [Integer]
+crece [] = []
+crece (x:xs) = (x+1) : crece  xs
+
+-- Funcion que resta cada vez un numero mas grande
+decre :: [Integer] -> [Integer]
+decre [] = []
+decre (x:xs) = (x-1) : decre xs
+
+sumatorio :: Integer -> Integer -> [Integer]
+sumatorio a b
+    |a >= b = []
+    |otherwise = crece listar
+    where listar = [a..b]
+
+
+--Ej: Escribir una expresión que denote la lista estrictamente decreciente de enteros que comienza
+--con el número 1 y termina con el número -100.
+
+decreciente :: Integer -> [Integer] -> [Integer]
+decreciente _ [] = []
+decreciente a (x:xs) = (x-a) : (decreciente a xs)
 
 --sumatoria :: [Integer] -> Integer
 --sumatoria xs = xs[0]
