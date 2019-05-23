3e20de243fd884f320517fc77d92dd4c1f0d11f2
mrfranac <13649860+franhace@users.noreply.github.com>
2019-05-23 10:36:57 -0300

    parcial

---
+++ Parciales/parcial.hs
@@ -0,0 +1,19 @@
+-- Ej 1
+-- Funcion que compara numeros por sus decenas
+-- y devuelve el minimo
+
+-- Fx que busca la decena
+decenas :: Integer -> Integer
+decenas n = div (mod n 100) 10
+
+minimo :: (Integer, Integer, Integer) -> Integer
+minimo (a,b,c) | decenas a < decenas b && decenas a < decenas c = a
+               | decenas a < decenas b && decenas c < decenas a = c
+               | otherwise = b
+
+-- Ej 2
+sucesion :: Integer -> Integer
+sucesion 1 = 1
+sucesion n = 1 + n*(sucesion(n-1)) - (n-1)*(n-1)
+
+-- Ej 3
