42bd71bbc97ae568be16dbd46bca70b8f1d824d0
mrfranac <13649860+franhace@users.noreply.github.com>
2019-05-29 18:39:03 -0300

    mas parcialeeees

---
+++ Algebra_Taller_parcial_14-10-16.hs
@@ -0,0 +1,29 @@
+-- Ej 1
+-- kesimo, devuelvo el kesimo elemento de una lista
+kesimo :: [Integer] -> Integer -> Integer
+kesimo xs k | k == 1 = head xs
+            | otherwise = kesimo (tail xs) (k-1)
+
+-- Ej 2
+-- domina devuelve True ssi cada elemento de xs es mayor al de ys en misma posicion
+domina :: [Integer] -> [Integer] -> Bool
+domina xs ys | length ys == 0 = False
+             | length ys == 1 = dominaAux xs ys
+             | length ys > 1 && ( dominaAux xs ys == False ) = False
+             | length ys > 1 && ( dominaAux xs ys == True ) = domina (tail xs) (tail ys)
+
+dominaAux :: [Integer] -> [Integer] -> Bool
+dominaAux xs ys | (head xs) > (head ys) = True
+                | otherwise = False
+
+-- Ej 3
+-- dada una lista de al menos 3 elementos, devuelve True si los 2 anteriores son iguales
+-- a la suma del 3ero
+esTipoAux :: [Integer] -> Bool
+esTipoAux xs | head xs + head (tail xs) != head (tail(tail xs)) = False
+             | head xs + head (tail xs) == head (tail(tail xs)) = True
+
+esTipoFibonacci :: [Integer] -> Bool
+esTipoFibonacci xs | length xs == 3 = esTipoAux xs
+                   | length xs > 3 && (head xs + head (tail xs) != head(tail(tail xs))) = False
+                   | length xs > 3 && (head xs + head (tail xs) != head(tail(tail xs))) = esTipoFibonacci (tail xs)
---
+++ README.md
@@ -0,0 +1,7 @@
+Recopilación de resoluciones de la guía de ejercicios y parciales del taller de la materia AlgebraI.
+
+Disclaimer: Aclaro que muchas de las soluciones son de mi propia autoría y no la solución "oficial". Si bien siempre busco que la solución sea lo más concisa y concreta posible, asi que seguro hay un par mejorables.
+
+Igualmente todas cumplen con los casos control.
+
+Recomiendo esta "guía" como último recurso en caso de que no salieran los ejercicios.
